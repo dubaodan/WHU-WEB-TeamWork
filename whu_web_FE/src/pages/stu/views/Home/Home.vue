56db49be1dd90298c65c83b17cbe64ef2fe4003b
ihavenoideayet <50798270+ihavenoideayet@users.noreply.github.com>
2020-04-05 01:52:47 +0800

    人体结构图

--- whu_web_FE/src/pages/stu/views/Home/Home.vue
+++ whu_web_FE/src/pages/stu/views/Home/Home.vue
@@ -1,5 +1,17 @@
 <template>
-<div>hwk</div>
+<div >
+  <div @click="skip('/Settings/Article')">
+  <img :src="require('@/assets/06.-Pecs.png')" style="    top:46%;
+    left: 25.2%;
+    vertical-align: middle;
+    width: 12.5%;
+    display: block;
+    position: absolute;" >
+  </div>
+  <div>
+  <img :src="require('@/assets/00.-Blank-Figures.png')"  style="width:100%; vertical-align: middle;">
+  </div>
+</div>
 </template>
 
 <script>
@@ -7,6 +19,11 @@ export default {
   name: 'Home',
   components: {
 
+  },
+  methods: {
+    skip (url) {
+      location.href = url
+    }
   }
 }
 </script>
