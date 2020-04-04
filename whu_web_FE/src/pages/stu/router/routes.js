56aef279e809ca1457d47696100f471f4e21eaee
Drbuilders <928910195@qq.com>
2020-04-04 23:33:39 +0800

    modify router

--- whu_web_FE/src/pages/stu/router/routes.js
+++ whu_web_FE/src/pages/stu/router/routes.js
@@ -5,6 +5,10 @@ import Register from '../components/Register-Login/Register'
 import Setting from '@/pages/stu/components/Customer-Center/Setting'
 import Account from '@/pages/stu/components/Customer-Center/Account'
 import Profile from '@/pages/stu/components/Customer-Center/Profile'
+import Settings from '@/pages/stu/components/Muscle-Center/Settings'
+import Article from '@/pages/stu/components/Muscle-Center/Article'
+import Video from '@/pages/stu/components/Muscle-Center/Video'
+import Discuss from '@/pages/stu/components/Muscle-Center/Discuss'
 import Prevention from '../views/Prevention/Prevention'
 import Food from '@/pages/stu/views/Food/Food'
 import Tools from '@/pages/stu/views/Tools/Tools'
@@ -50,6 +54,23 @@ export default[
         component: Profile
       }
     ]
+  },
+  {
+    path: '/Settings',
+    component: Settings,
+    children: [
+      {
+        path: 'Article',
+        component: Article
+      },
+      {
+        path: 'Video',
+        component: Video
+      },
+      {
+        path: 'Discuss',
+        component: Discuss
+      }
+    ]
   }
-
 ]
