2dc7a3241873515aee2f02aae5deea68714a4102
Wenke_Huang <591852399@qq.com>
2020-04-22 14:39:05 +0800

    4.22

--- whu_web_FE/src/pages/stu/components/Tools/Tools.vue
+++ whu_web_FE/src/pages/stu/components/Tools/Tools.vue
@@ -1,13 +1,59 @@
 <template>
-<div></div>
+  <el-card >
+    <img :src=toolInfo.ImgSrc class="image">
+    <div style="padding: 10px;">
+      <span class="Title">{{toolInfo.Title}}</span>
+      <div class="bottom clearfix">
+        <time class="Description">{{toolInfo.Description}}</time>
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script>
 export default {
-  name: 'Tools'
+  name: 'Tools',
+  props: {
+    toolInfo: {
+      ImgSrc: String,
+      Title: String,
+      Description: String
+    }
+  },
+  data () {
+    return {
+      imgsrc: this.toolInfo.ImgSrc
+    }
+  },
+  methods: {
+  }
 }
 </script>
 
 <style scoped>
-
+  .el-card{
+    margin: 10px;
+    width: 200px;
+    height: 300px;
+  }
+  .Description {
+    font-size: 10px;
+  }
+  .Title {
+    font-size: 20px
+  }
+  .bottom {
+  }
+  .image {
+    width: 80%;
+    padding: auto;
+  }
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
 </style>
