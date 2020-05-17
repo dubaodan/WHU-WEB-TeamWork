f23913578bbf94f178eef1268ec995bc61532f60
Wenke_Huang <591852399@qq.com>
2020-05-17 20:30:58 +0800

    5.17 update

--- whu_web_FE/src/pages/stu/views/Prevention/Prevention.vue
+++ whu_web_FE/src/pages/stu/views/Prevention/Prevention.vue
@@ -25,38 +25,20 @@ export default {
         {
           ImgSrc: 'https://s1.ax1x.com/2020/04/24/JrB9f0.jpg',
           Title: '腰肌劳损',
-          Name: 'LumbarMuscleDegeneration',
-          Description: '因腰部肌肉、韧带等软组织损伤导致疼痛 \n 好发于30～50岁的中青年人 \n 可表现出腰部疼痛及活动受限 \n 积极治疗后大多数可得到完全缓解'
+          Name: '腰肌劳损',
+          Description: '腰肌劳损，又称功能性腰痛、慢性下腰损伤、腰臀肌筋膜炎等，是腰痛的常见原因之一，主要症状是腰或腰骶部胀痛、酸痛，反复发作，疼痛可随气候变化或劳累程度而变化，为临床常见病，多发病。 '
         },
         {
           ImgSrc: 'https://s1.ax1x.com/2020/04/27/JWDQsA.jpg',
-          Title: '筋膜炎',
-          Name: 'Fasciitis',
-          Description: '一种筋膜的无菌性炎症 \n 可能与劳损、潮湿、免疫因素等有关 \n 可有局部疼痛、肌肉痉挛、运动障碍等 \n 保守治疗为主，适当情况下可考虑手术'
+          Title: '肌肉拉伤',
+          Name: '肌肉拉伤',
+          Description: '肌肉拉伤是肌肉在运动中急剧收缩或过度牵拉引起的损伤。这在引体向上和仰卧起坐练习时容易发生。肌肉拉伤后，拉伤部位剧痛，用手可摸到肌肉紧张形成的索条状硬块，触疼明显，局部肿胀或皮下出血，活动明显受到限制。'
         },
         {
-          ImgSrc: 'https://s1.ax1x.com/2020/04/24/JrB9f0.jpg',
-          Title: '腰肌劳损',
-          Name: 'LumbarMuscleDegeneration',
-          Description: '因腰部肌肉、韧带等软组织损伤导致疼痛 \n 好发于30～50岁的中青年人 \n 可表现出腰部疼痛及活动受限 \n 积极治疗后大多数可得到完全缓解'
-        },
-        {
-          ImgSrc: 'https://s1.ax1x.com/2020/04/27/JWDQsA.jpg',
-          Title: '筋膜炎',
-          Name: 'Fasciitis',
-          Description: '一种筋膜的无菌性炎症 \n 可能与劳损、潮湿、免疫因素等有关 \n 可有局部疼痛、肌肉痉挛、运动障碍等 \n 保守治疗为主，适当情况下可考虑手术'
-        },
-        {
-          ImgSrc: 'https://s1.ax1x.com/2020/04/24/JrB9f0.jpg',
-          Title: '腰肌劳损',
-          Name: 'LumbarMuscleDegeneration',
-          Description: '因腰部肌肉、韧带等软组织损伤导致疼痛 \n 好发于30～50岁的中青年人 \n 可表现出腰部疼痛及活动受限 \n 积极治疗后大多数可得到完全缓解'
-        },
-        {
-          ImgSrc: 'https://s1.ax1x.com/2020/04/27/JWDQsA.jpg',
-          Title: '筋膜炎',
-          Name: 'Fasciitis',
-          Description: '一种筋膜的无菌性炎症 \n 可能与劳损、潮湿、免疫因素等有关 \n 可有局部疼痛、肌肉痉挛、运动障碍等 \n 保守治疗为主，适当情况下可考虑手术'
+          ImgSrc: 'https://s1.ax1x.com/2020/05/17/YRKNKx.jpg',
+          Title: '骨质疏松症',
+          Name: '骨质疏松症',
+          Description: '骨质疏松症（osteoporosis）是由于多种原因导致的骨密度和骨质量下降，骨微结构破坏，造成骨脆性增加，从而容易发生骨折的全身性骨病。'
         }
       ]
     }
